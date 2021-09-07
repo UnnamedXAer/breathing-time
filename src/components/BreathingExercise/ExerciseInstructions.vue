c8f202811792e87253a433311af50c093252a402
UnnamedXAer <UnnamedXAer@gmail.com>
2021-09-07 09:24:39 +0200

    update

--- src/components/BreathingExercise/ExerciseInstructions.vue
+++ src/components/BreathingExercise/ExerciseInstructions.vue
@@ -1,30 +1,71 @@
 <template>
-  <article class="round-phases">
+  <article class="round_phases">
     <h2>Round Phaes</h2>
     <ol>
-      <li>30 - 50 deep breaths.</li>
-      <li>Hold your breath on the exhale until stong need to inhale.</li>
-      <li>Recovery - Hold your breath on the inhle for 15 s.</li>
+      <li>Breathing - take 30 - 50 deep breaths.</li>
+      <li>
+        Breath hold - exhale then stop breathing until you feel urge to inhale.
+      </li>
+      <li>Recovery - inhale deeply and hold it for 15 s.</li>
     </ol>
   </article>
+
+  <article class="warning_note">
+    <h2>Attention</h2>
+    <p>
+      This breathing exercises can affect your motor control, cause temporary
+      dizziness or even make you faint - do NOT do it while driving or in kind
+      of dangerous places. Find safe place like sofa or bed.
+    </p>
+  </article>
+
   <article class="instructions">
     <h2>Exercise instructions</h2>
     <ol>
       <li>
-        Get comfortable - lie down or sit depending what is the moste
-        comfortable for you.
+        <p>
+          Get comfortable - lie down or sit depending what is the most
+          comfortable for you, make sure you can breath freely - your lungs can
+          expand without any constriction. Any additional muscle constraction
+          will hinder the exercise and shorten the duration of your breath
+          holding.
+        </p>
       </li>
       <li>
-        Take 30 - 50 deep breaths - inhale deeply throught nose or mouth and
-        exhale throught mouth. Try to unhale to the belly. Do not force exhale.
+        <p>
+          Take 30 - 50 deep breaths - inhale deeply through nose or mouth and
+          exhale through mouth. Try to inhale deeply to the belly. Do not force
+          exhale, just let the air go.
+        </p>
+        <p>
+          You may expirience dizzines, tingling and numbness in fingers and legs
+          - these are normal, harmless side effects of lowering the level of
+          carbon dioxide in the blood stream.
+        </p>
       </li>
       <li>
-        Hold breath - On the last breath ihhale really deeply and let the air go
-        - stop breathing. Hold on breath out until you feel strong need to
-        breath.
+        <p>
+          Hold breath - after the last breath take deep breath - fill whole
+          lungs and let the air go - stop breathing. Hold breath until you feel
+          urge to inhale.
+        </p>
+        <p>
+          Stop breathing may cause fainting if you force it to much. It is not
+          dangerous but better avoided. Holding breath until the feel of
+          moderate or strong need to inhale is enough.
+        </p>
       </li>
-      <li>Recover - Hold breath - Inhale once and stop breathing for 15 s.</li>
-      <li>Round is completed. Nex round will be started.</li>
+      <li>
+        <p>
+          Recovery - once you feel urge to breath inhale at full capacity of
+          your lungs and stop breathing for about 15 seconds. One round is
+          completed.
+        </p>
+        <p>
+          The next round starts from the breathing phase. Repeat this cycle 3-5
+          times with not break in between.
+        </p>
+      </li>
     </ol>
   </article>
 </template>
