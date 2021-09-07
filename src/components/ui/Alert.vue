ad0192557475219ca2e592916e21b01b3127686b
UnnamedXAer <UnnamedXAer@gmail.com>
2021-09-07 11:17:11 +0200

    update alert
    improves ui
    adds icons

--- src/components/ui/Alert.vue
+++ src/components/ui/Alert.vue
@@ -1,16 +1,36 @@
 <template>
-  <div role="alert" class="alert" :class="{ [mode]: true, [variant]: true }">
-    <slot></slot>
-    <button v-if="!!dismiss" aria-label="close warning" @click="dismiss">
-      &times;
-    </button>
+  <div
+    role="alert"
+    class="alert"
+    :class="{
+      [mode]: true,
+      [variant]: true,
+      dismissable: !!dismiss,
+      with_icon: icon,
+    }"
+  >
+    <component :is="icon" :aria-label="mode + ' icon'" class="alert_icon" />
+    <span style="flex: 1"><slot></slot></span>
+    <div v-if="!!dismiss" class="close_btn__wrapper">
+      <button class="close_btn" aria-label="close warning" @click="dismiss">
+        &times;
+      </button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
+import InfoIconSvgVue from "../svg/InfoIconSvg.vue";
+import WarningIconSvgVue from "../svg/WarningIconSvg.vue";
+import DangerIconSvgVue from "../svg/DangerIconSvg.vue";
 
 export default defineComponent({
+  components: {
+    appWarningIcon: WarningIconSvgVue,
+    appInfoIcon: InfoIconSvgVue,
+    appDangerIcon: DangerIconSvgVue,
+  },
   props: {
     dismiss: Function,
     mode: {
@@ -20,8 +40,35 @@ export default defineComponent({
       default: "normal",
     },
     variant: {
-      type: String as PropType<"contained" | "outlined" | "link">,
+      type: String as PropType<"contained" | "outlined">,
       default: "contained",
+    },
+    showIcon: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  computed: {
+    icon() {
+      if (!this.showIcon) {
+        return null;
+      }
+      let icon: string | null = null;
+
+      switch (this.mode) {
+        case "info":
+          icon = "app-info-icon";
+          break;
+        case "warning":
+          icon = "app-warning-icon";
+          break;
+        case "danger":
+          icon = "app-danger-icon";
+          break;
+      }
+
+      console.log(icon);
+      return icon;
     },
   },
 });
@@ -29,15 +76,36 @@ export default defineComponent({
 
 <style scoped>
 .alert {
-  box-sizing: border-box;
+  text-align: start;
+  position: relative;
   padding: 1rem 1.1rem;
   font-size: 1rem;
-  border: none;
   font-weight: 600;
   color: white;
   background-color: rgb(0, 145, 230);
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+  min-width: 300px;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+@media screen and (min-width: 540px) {
+  .alert {
+    min-width: 300px;
+    width: initial;
+  }
+}
+
+.alert.dismissable {
+  padding-right: 0.3rem;
+}
+
+.alert.with_icon {
+  padding-left: 0.3rem;
 }
 
 .alert.outlined {
@@ -82,8 +150,14 @@ export default defineComponent({
   background-color: rgb(200, 150, 0);
 } */
 
-.alert > .spinner {
-  margin-left: -0.9rem;
-  margin-right: 0.3rem;
+.alert .close_btn__wrapper {
+  display: flex;
+}
+
+.alert .close_btn {
+  background-color: inherit;
+  border: none;
+  font-size: 2rem;
+  color: inherit;
 }
 </style>
