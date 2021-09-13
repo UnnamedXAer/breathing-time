54082bfa4442cee1f629642f0a29e942e464c401
UnnamedXAer <UnnamedXAer@gmail.com>
2021-09-13 12:08:12 +0200

    replace default values with user preferences

--- src/components/BreathingExercise/RoundPhases.vue
+++ src/components/BreathingExercise/RoundPhases.vue
@@ -1,17 +1,28 @@
 <template>
   <article class="round_phases">
     <h2>Round's phases</h2>
-    <ol>
-      <li>Breathing - take 30 - 50 deep breaths.</li>
+    <ol style="line-height: 2em">
+      <li>Breathing - take {{ breathsPerRound }} deep breaths.</li>
       <li>
         Breath hold - exhale then stop breathing until you feel urge to inhale.
       </li>
-      <li>Recovery - inhale deeply and hold breath for 15 seconds.</li>
+      <li>
+        Recovery - inhale deeply and hold breath for {{ recoveryTime }} seconds.
+      </li>
     </ol>
   </article>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-export default defineComponent({});
+export default defineComponent({
+  computed: {
+    breathsPerRound() {
+      return this.$store.state.exercise.breathsPerRound;
+    },
+    recoveryTime() {
+      return this.$store.state.exercise.recoveryTime;
+    },
+  },
+});
 </script>
