e298dd696fca1f4a4f637eec5de3cf2172dbcbbc
UnnamedXAer <UnnamedXAer@gmail.com>
2021-09-13 12:38:15 +0200

    update ui and fix missing computed prop

--- src/views/BreathingExerciseSummary.vue
+++ src/views/BreathingExerciseSummary.vue
@@ -7,17 +7,21 @@
       >
       and start your breathing.
     </h2>
-    <h2 v-else-if="holdTimes.length === rounds">
+    <h2 v-else-if="holdTimes.length === numberOfRounds">
       Congrats you finished your breathing!
     </h2>
     <h2 v-else-if="holdTimes.length === 0">You did not finish any rounds.</h2>
     <h2 v-else>Breathing finished.</h2>
-    <table>
+    <table class="summary__results_table">
       <tr v-for="(time, idx) in holdTimes" :key="idx">
         <th>Round {{ idx + 1 }}</th>
         <td>{{ time }} s</td>
       </tr>
     </table>
+    <p v-if="holdTimes.length > 1 && fromRecovery">
+      Average Time:
+      <strong style="font-size: 1.1em">{{ averageTime }}</strong> seconds.
+    </p>
   </article>
 </template>
 
@@ -42,9 +46,17 @@ export default defineComponent({
   },
   computed: {
     ...(mapState<StoreState>("exercise", exerciseStateProps) as ComputedTypes),
+    averageTime() {
+      if (this.holdTimes.length === 0) {
+        return 0;
+      }
+
+      return (
+        this.holdTimes.reduce((pv, v) => pv + v) / this.holdTimes.length
+      ).toFixed(3);
+    },
   },
   mounted() {
-    console.log("fromRecovery", this.fromRecovery);
     this.$store.commit(
       namespaceName("exercise", ExerciseMutations.SetRoundState),
       RoundState.Stopped
@@ -52,3 +64,28 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.summary {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.summary__results_table {
+  font-size: 1.3em;
+  border-spacing: 0;
+}
+
+.summary__results_table th {
+  padding: 0.5rem 3rem;
+}
+
+.summary__results_table td {
+  padding-right: 3rem;
+}
+
+.summary__results_table tr:nth-child(even) {
+  background-color: rgb(240, 240, 240);
+}
+</style>
