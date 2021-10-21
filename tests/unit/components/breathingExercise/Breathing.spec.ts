2296d9aff8c55df68dccb9784f4a2df1aa9d685a
UnnamedXAer <UnnamedXAer@gmail.com>
2021-10-21 15:10:27 +0200

    add tests

--- tests/unit/components/breathingExercise/Breathing.spec.ts
+++ tests/unit/components/breathingExercise/Breathing.spec.ts
@@ -9,6 +9,9 @@ import LeaveExerciseConfirm from "@/components/BreathingExercise/LeaveExerciseCo
 import Counter from "@/components/BreathingExercise/counter/Counter.vue";
 import Footer from "@/components/BreathingExercise/Footer.vue";
 import { StoreState } from "@/store/types";
+import { RouteLocationNormalized } from "vue-router";
+import MixinLeaveExercise from "@/components/BreathingExercise/MixinLeaveExercise.vue";
+import Modal from "@/components/modal/Modal.vue";
 
 describe("Breathing Exercise / Breathing.vue", () => {
   it("renders correctly", async () => {
@@ -44,7 +47,7 @@ describe("Breathing Exercise / Breathing.vue", () => {
 
     showStartTip = false;
     await wrapper.setData({ showStartTip: showStartTip });
-    console.log("showStartTip", showStartTip);
+
     counterWrapper = wrapper.findComponent(Counter);
     expect(counterWrapper.exists()).eq(!showStartTip);
   });
@@ -119,7 +122,7 @@ describe("Breathing Exercise / Breathing.vue", () => {
     });
   });
 
-  it("calls next screen when counter reaches 'breath per round'", async () => {
+  it("calls next screen when counter reaches 'breaths per round'", async () => {
     const store = createStoreFactory();
     const state = store.state as StoreState;
     (state as StoreState).exercise = {
@@ -152,6 +155,134 @@ describe("Breathing Exercise / Breathing.vue", () => {
     expect(replaceSpy).to.have.been.called.once;
     expect(replaceSpy).to.have.been.called.with({
       name: "BreathingExercise-BreathHold",
+    });
+  });
+
+  it("shows exercise leave confirmation if try to navigate from 'exercise' routes", async () => {
+    const store = createStoreFactory();
+    const state = store.state as StoreState;
+    (state as StoreState).exercise = {
+      ...state.exercise,
+      disableStartTips: true,
+    };
+
+    const replaceSpy = chai.spy();
+
+    const wrapper = mount(Breathing, {
+      data() {
+        return {
+          showStartTip: false,
+          counter: 5,
+        };
+      },
+      global: {
+        plugins: [store],
+        mixins: [MixinLeaveExercise],
+        mocks: {
+          $router: {
+            replace: replaceSpy,
+          },
+        },
+      },
+    });
+
+    const routeName = "Home";
+
+    expect(wrapper.vm.showModal).to.be.false;
+    expect(wrapper.vm.allowNavigation).to.be.false;
+    expect(wrapper.vm.routeToNavigate).to.be.null;
+    expect(wrapper.findComponent(LeaveExerciseConfirm).exists()).to.be.false;
+
+    const to = {
+      name: routeName,
+      params: {},
+    } as RouteLocationNormalized;
+    const from = {} as RouteLocationNormalized;
+    const next = () => void 0;
+
+    expect(Breathing.beforeRouteLeave).to.not.be.undefined;
+
+    expect(Breathing.beforeRouteLeave).to.not.be.undefined;
+    if (!Breathing.beforeRouteLeave) {
+      return;
+    }
+
+    Breathing.beforeRouteLeave.call(wrapper.vm, to, from, next);
+
+    expect(wrapper.vm.showModal).to.be.true;
+    expect(wrapper.vm.allowNavigation).to.be.false;
+    expect(wrapper.vm.routeToNavigate).eq(routeName);
+
+    await wrapper.vm.$nextTick();
+
+    const leaveExerciseConfirmWrapper =
+      wrapper.findComponent(LeaveExerciseConfirm);
+    expect(leaveExerciseConfirmWrapper.exists()).to.be.true;
+  });
+
+  it.only("leaves exercise if user confirms dialog", async () => {
+    const store = createStoreFactory();
+    const state = store.state as StoreState;
+    (state as StoreState).exercise = {
+      ...state.exercise,
+      disableStartTips: true,
+    };
+
+    const replaceSpy = chai.spy();
+
+    const wrapper = mount(Breathing, {
+      attachTo: document.body,
+      data() {
+        return {
+          showStartTip: false,
+          counter: 5,
+        };
+      },
+      global: {
+        plugins: [store],
+        mixins: [MixinLeaveExercise],
+        mocks: {
+          $router: {
+            replace: replaceSpy,
+          },
+        },
+      },
+    });
+
+    const routeName = "Home";
+
+    const to = {
+      name: routeName,
+      params: {},
+    } as RouteLocationNormalized;
+    const from = {} as RouteLocationNormalized;
+    const next = () => void 0;
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    Breathing.beforeRouteLeave!.call(wrapper.vm, to, from, next);
+    await wrapper.vm.$nextTick();
+
+    // LeaveExerciseConfirm - use underling modal component
+    // to capture correct element (html).
+    const leaveExerciseWrapper = wrapper.findComponent(Modal);
+
+    const actionWrappers = leaveExerciseWrapper.findAll(
+      '[data-test="actions"] [data-test="btn"]'
+    );
+
+    const confirmActionWrapper = actionWrappers.find((x) =>
+      x.text().includes("yes")
+    );
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await confirmActionWrapper!.trigger("click");
+
+    expect(replaceSpy).to.be.called.once;
+    expect(replaceSpy).to.be.called.with({
+      name: to.name,
+      params: {
+        allowNavigation: 1,
+      },
     });
   });
 });
