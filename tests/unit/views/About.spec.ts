1a173e670011a1d79a5f764069c0d20e206744c6
UnnamedXAer <UnnamedXAer@gmail.com>
2021-11-04 14:49:34 +0100

    fix test

--- tests/unit/views/About.spec.ts
+++ tests/unit/views/About.spec.ts
@@ -1,5 +1,5 @@
 import About from "@/views/About.vue";
-import { mount, shallowMount, VueWrapper } from "@vue/test-utils";
+import { mount, VueWrapper } from "@vue/test-utils";
 import { expect } from "chai";
 
 describe("About.vue - view", () => {
@@ -13,23 +13,19 @@ describe("About.vue - view", () => {
     }
   });
 
-  it.only("renders correctly", () => {
+  it("renders correctly", () => {
     wrapper = mount(About, {
       global: {
         stubs: {
-          "i18n-t": {
-            template: '<p data-test="about-stub-i18n-t" ></p>',
-          },
-          // "router-link": true,
+          "router-link": true,
         },
       },
     });
 
-    console.log("-?>>>>", wrapper.html());
-
     const headerWrapper = wrapper.get('[data-test="about-title"]');
     const contentWrapper = wrapper.get('[data-test="about-content"]');
     const appVersionWrapper = wrapper.get('[data-test="about-version"]');
+
     expect(headerWrapper.text()).eq("about.title");
     expect(contentWrapper.text()).to.not.be.empty;
     expect(appVersionWrapper.text()).includes(process.env.APP_VERSION);
