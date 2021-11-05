a4da475c1da178e16c150d6eba7d0091328c2593
UnnamedXAer <UnnamedXAer@gmail.com>
2021-11-05 15:17:24 +0100

    check e2e tests setup

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,8 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("My First Test", () => {
-  it("Visits the app root url", () => {
+  it("Visits the app root url", function () {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js + TypeScript App");
+    cy.contains("h1", this.packageJson.appTitle);
   });
 });
